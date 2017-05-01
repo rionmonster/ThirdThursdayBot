0725c08dc56fb72236516dd3c1948de69fb49c48
Rion Williams <rionmonster@gmail.com>
2017-05-01 17:21:19 -0500

    Hide your children, this code is dirty.

--- src/ThirdThursdayBot/Controllers/MessagesController.cs
+++ src/ThirdThursdayBot/Controllers/MessagesController.cs
@@ -4,12 +4,33 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.Bot.Connector;
 using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using Firebase.Database;
+using Firebase.Database.Query;
+using ThirdThursdayBot.Models;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace ThirdThursdayBot
 {
+    /// <summary>
+    /// NOTE: This is super dirty code that will need to be cleaned up
+    /// </summary>
+
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+        private HttpClient _client;
+
+        public MessagesController()
+        {
+            _client = new HttpClient()
+            {
+                BaseAddress = new Uri("todo:put-me-in-azure")
+            };
+        }
+
         /// <summary>
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
@@ -19,47 +40,81 @@ namespace ThirdThursdayBot
             ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
             if (activity.Type == ActivityTypes.Message)
             {
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                // See if our response matched any of our options, if not display messages again
+                var message = activity.Text.Trim().ToLower();
+
+                if (Regex.IsMatch(message, "show|all", RegexOptions.IgnoreCase))
+                {
+                    var reply = activity.CreateReply(await BuildListOfVisitedRestaurantsAsync());
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
+                else if (Regex.IsMatch(message, "have we been to", RegexOptions.IgnoreCase))
+                {
+                    // Get the parameter
+                    var target = message.Replace("have we been to", "");
+
+                    // Attempt to query firebase to see if there's a match
+
+                    // If so, let them know when and who picked it
+                    // Get the last pick and then resolve who is next from Firebase
+                    var reply = activity.CreateReply("'' has not been chosen.");
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+
+                }
+                else if (Regex.IsMatch(message, "who's turn|who is next", RegexOptions.IgnoreCase))
+                {
+                    // Get the last pick and then resolve who is next from Firebase
+                    var reply = activity.CreateReply("It is James' turn on ''");
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
+                else
+                {
+                    var reply = activity.CreateReply(GetDefaultResponse());
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
             }
-            else
-            {
-                HandleSystemMessage(activity);
-            }
+
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
 
-        private Activity HandleSystemMessage(Activity message)
+        private string GetDefaultResponse()
         {
-            if (message.Type == ActivityTypes.DeleteUserData)
+            // Build message
+            var message = new StringBuilder($"Hi, I'm Third Thursday Bot! I support the following commands: {Environment.NewLine}");
+            var options = new[] {
+                    "'show all' Lists all of the previous Third Thursday selections.",
+                    "'have we been to '{location}' Indicates if specific restaurant has been chosen.",
+                    "'who's turn is it' - Indicates who has the next selection.",
+            };
+
+            foreach (var option in options)
             {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
+                message.AppendLine($"- {option}");
             }
 
-            return null;
+            return message.ToString();
+        }
+
+        private async Task<string> BuildListOfVisitedRestaurantsAsync()
+        {
+            try
+            {
+                var json = await _client.GetStringAsync("/Restaurants/.json");
+                var restaurants = JsonConvert.DeserializeObject<Restaurant[]>(json);
+
+                var message = new StringBuilder($"The following restaurants have been visited: {Environment.NewLine}");
+                foreach (var restaurant in restaurants)
+                {
+                    message.AppendLine($"- '{restaurant.Location}' ({restaurant.PickedBy})");
+                }
+
+                return message.ToString();
+            }
+            catch(Exception ex)
+            {
+                return ex.ToString();
+            }
         }
     }
 }
---
+++ src/ThirdThursdayBot/Models/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ThirdThursdayBot.Models
+{
+    public class Restaurant
+    {
+
+        [JsonProperty("Location")]
+        public string Location { get; private set; }
+
+        [JsonProperty("PickedBy")]
+        public string PickedBy { get; private set; }
+
+        [JsonProperty("Date")]
+        public string Date { get; private set; }
+
+    }
+}
