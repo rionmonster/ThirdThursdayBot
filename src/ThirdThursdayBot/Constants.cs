920bd232645ce18c74b5a7292569c4e7ce9c935f
Rion Williams <rionmonster@gmail.com>
2017-05-02 00:12:49 -0500

    More Message Tweaking

--- src/ThirdThursdayBot/Constants.cs
+++ src/ThirdThursdayBot/Constants.cs
@@ -5,22 +5,22 @@ namespace ThirdThursdayBot
     public class Constants
     {
         public readonly static string DefaultResponseMessage =
-            $@"Hi, I'm Third Thursday Bot! I support the following commands: {Environment.NewLine}
-            - 'show all' Lists all of the previous Third Thursday selections. {Environment.NewLine}
-            - 'have we been to {{restaurant}}?' Indicates if specific restaurant has been chosen. {Environment.NewLine}
-            - 'who's next' - Indicates who has the next selection. {Environment.NewLine}";
+            $"Hi, I'm Third Thursday Bot! I support the following commands: {Environment.NewLine}" +
+            $"- 'show all' Lists all of the previous Third Thursday selections. {Environment.NewLine}" +
+            $"- 'have we been to {{restaurant}}?' Indicates if specific restaurant has been chosen. {Environment.NewLine}" +
+            $"- 'who's next' - Indicates who has the next selection. {Environment.NewLine}";
 
         public readonly static string RestaurantListingMessage =
             $"All of the following restaurants have been visited: {Environment.NewLine}";
 
         public const string NextChooserFormattingMessage =
-            @"{0} has the next choice for {1:MMM}.";
+            @"{0} has the next choice for {1:MMMM}.";
 
         public const string UnchosenRestaurantFormattingMessage =
             @"{0} has not been chosen before. Give it a shot!";
 
         public const string PreviouslyChosenResturantFormattingMessage =
-            @"{0} was chosen by {1} in {2:MMM} of {2:yyyy}";
+            @"{0} was chosen by {1} in {2:MMMM} of {2:yyyy}";
 
         public const string UnrecognizableRestaurantMessage =
             @"Sorry, I couldn't figure out what restaurant you were looking for. Try again.";
